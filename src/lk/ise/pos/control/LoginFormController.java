652b0d966c57a3317aba18ac7b43aa705006fd6d
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2022-12-31 09:53:33 +0530

    completed

--- src/lk/ise/pos/control/LoginFormController.java
+++ src/lk/ise/pos/control/LoginFormController.java
@@ -1,11 +1,13 @@
 package lk.ise.pos.control;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import lk.ise.pos.db.Database;
 import lk.ise.pos.entity.User;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class LoginFormController {
     public AnchorPane loginFormContext;
@@ -16,6 +18,18 @@ public class LoginFormController {
     }
 
     public void loginOnAction(ActionEvent actionEvent) {
-        Database.users.stream().forEach(user -> {});
+        User selectedUser = Database.users.stream()
+                .filter(user -> user.getUsername()
+                        .equals(txtUsername.getText()))
+                .findFirst().orElse(null);
+        if (selectedUser!=null){
+            if (BCrypt.checkpw(pwd.getText(),selectedUser.getPassword())){
+                System.out.println("user logged");
+            }else{
+                System.out.println("Wrong password!");
+            }
+        }else{
+            new Alert(Alert.AlertType.WARNING, "User name not found!").show();
+        }
     }
 }
