c8521fc0bb1d5984afc2414ca6c0ddaa9d3acced
Sanduni Wijesinghe <73924997+sanduniWijesinghe@users.noreply.github.com>
2023-01-07 09:17:55 +0530

    15

--- src/lk/ise/pos/control/PlaceOrderFormController.java
+++ src/lk/ise/pos/control/PlaceOrderFormController.java
@@ -1,4 +1,43 @@
 package lk.ise.pos.control;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import lk.ise.pos.db.Database;
+import lk.ise.pos.entity.Customer;
+
 public class PlaceOrderFormController {
+    public TextField txtCustomerName;
+    public ComboBox<String> cmbCustomerId;
+    public TextField txtAddress;
+    public TextField txtSalary;
+
+    public void initialize(){
+        loadCustomerIds();
+
+        cmbCustomerId.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue!=null){
+                setCustomerData(newValue);
+            }
+        });
+
+    }
+    private void setCustomerData(String id){
+        Customer customer =
+                Database.customers.stream().filter(e -> e.getId().equals(id)).findFirst().orElse(null);
+        if (customer!=null){
+            txtCustomerName.setText(customer.getName());
+            txtAddress.setText(customer.getAddress());
+            txtSalary.setText(String.valueOf(customer.getSalary()));
+        }else{
+            new Alert(Alert.AlertType.WARNING,"Not Found").show();
+        }
+    }
+
+    private void loadCustomerIds() {
+        for (Customer data: Database.customers){
+            cmbCustomerId.getItems().add(data.getId());
+        }
+    }
+
 }
--- src/lk/ise/pos/db/Database.java
+++ src/lk/ise/pos/db/Database.java
@@ -19,6 +19,10 @@ public class Database {
         users.add(new User("anna",encryptPassword("1234")));
         users.add(new User("tom",encryptPassword("1234")));
 
+        customers.add(new Customer("C001","Nimal","Colombo",25000));
+        customers.add(new Customer("C002","Jagath","Panadura",45634));
+        customers.add(new Customer("C003","Saman","Galle",23444));
+
         Item item1 = new Item("D-001","Description 1",25,2500);
         Item item2 = new Item("D-002","Description 2",34,4355);
         Item item3 = new Item("D-003","Description 3",20,2234);
